654e5afb81232f351c54e4e4208dabd610e677bf
Eunicepris <47212400+Eunicepris@users.noreply.github.com>
2019-11-18 13:56:04 +0000

    Insertion de templates

--- quizznan/quiz/urls.py
+++ quizznan/quiz/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home'),
+    path('', views.login, name='login'),
+    path('home', views.home, name='home'),
+    path('profil', views.profil, name='profil'),
+    path('quizz', views.quizz, name='quizz'),
 ]
--- quizznan/quiz/views.py
+++ quizznan/quiz/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# def home(request):
-#     return render(request, '')
+def login(request):
+    return render(request, 'pages/login.html')
+
+def home(request):
+    return render(request, 'pages/index.html')
+
+def profil(request):
+    return render(request, 'pages/profil.html')
+
+def quizz(request):
+    return render(request, 'pages/quizz.html')
