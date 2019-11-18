5a4b64b8247c64b56d6f4bf2601a58b85813adaa
Sedrickgael <kanangamansedrickgael@gmail.com>
2019-11-18 16:33:09 +0000

    login with vuejs

--- quizznan/quiz/views.py
+++ quizznan/quiz/views.py
@@ -1,12 +1,49 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+import json
 from . import models
 
 # Create your views here.
 
-def login(request):
+def connexion(request):
     return render(request, 'pages/login.html')
+
+def islogin(request,):
+    
+    postdata = json.loads(request.body.decode('utf-8'))
+    
+    # name = postdata['name']
+
+    username = postdata['username']
+    password = postdata['password']
+    isSuccess=False
+
+    user = authenticate(username=username, password=password)
+    
+    if user is not None and user.is_active:
+        print("user is login")
+        isSuccess = True
+        login(request, user)
+        datas = {
+        'success':True,
+        'username':username,
+        'message':'Votre connection a reussi avec succes',
+    }
+        return JsonResponse(datas,safe=False) # page si connect
+        
+    else:
+        data = {
+        'success':False,
+        'message':'Vos identifiants ne sont pas correcte',
+        }
+        return JsonResponse(data,safe=False)
+    
+    
+    
+    return JsonResponse(datas, safe=False)
+
 
 def home(request):
     return render(request, 'pages/index.html')
